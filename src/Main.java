3888c53f9f8d5e289cfc88e07b3485693a7a9307
thelegend2004 <oleh.liubichenko@gmail.com>
2025-03-20 15:59:53 +0200

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Main {
+    public static List<String> notes = new ArrayList<>();
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Console Todo List!");
+
+    }
+}
