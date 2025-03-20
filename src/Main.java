001abaf064d6520acc0e61427abc9323a12026e4
thelegend2004 <oleh.liubichenko@gmail.com>
2025-03-20 16:30:21 +0200

    Add interface to use program (CRUD).

--- src/Main.java
+++ src/Main.java
@@ -4,32 +4,68 @@ import java.util.Scanner;
 
 public class Main {
     public static List<String> notes = new ArrayList<>();
+    private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Console Todo List!");
-        Scanner scanner = new Scanner(System.in);
+        while(true) {
+            System.out.println("What do you want to do?");
+            int num = scanner.nextInt();
+            scanner.nextLine();
+            switch (num) {
+                case 1:
+                    addNote();
+                    break;
+                case 2:
+                    removeNote();
+                    break;
+                case 3:
+                    showNote();
+                    break;
+                case 4:
+                    showNotesIds();
+                    break;
+                default:
+                    return;
+            }
+        }
     }
 
-    public static void addNote(String note) {
+    public static void addNote() {
+        System.out.println("Please write a note");
+        String note = scanner.nextLine();
         notes.add(note);
-        int id = notes.indexOf(note);
+        int id = notes.lastIndexOf(note);
         System.out.println("Note #" + id + " has been successfully added!");
     }
 
-    public static void removeNote(int id) {
-        notes.remove(id);
-        System.out.println("Note #" + id + " has been successfully removed!");
+    public static void removeNote() {
+        System.out.println("Please write a note id which you want to remove");
+        int id = scanner.nextInt();
+        try {
+            notes.remove(id);
+            System.out.println("Note #" + id + " has been successfully removed!");
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Note #" + id + " does not exist!");
+        }
     }
 
-    public static void showNote(int id) {
-        String note = notes.get(id);
-        System.out.println(note);
+    public static void showNote() {
+        System.out.println("Please write a note id which you want to view");
+        int id = scanner.nextInt();
+        try {
+            String note = notes.get(id);
+            System.out.println(note);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Note #" + id + " does not exist!");
+        }
     }
 
     public static void showNotesIds() {
+        System.out.println("All notes:");
         for(String note : notes) {
             System.out.println(notes.indexOf(note));
-            System.out.println(note.substring(0, 10));
+            System.out.println(note.substring(0, Math.min(note.length(), 5)));
             System.out.println();
         }
     }
