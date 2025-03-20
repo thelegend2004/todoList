8e85fc6f20bc8931c5636f7f12ef6084904ffd2a
thelegend2004 <oleh.liubichenko@gmail.com>
2025-03-20 16:12:48 +0200

    Change readme.md. Add note methods to main.

--- readme.md
+++ readme.md
@@ -11,7 +11,11 @@ java src/Main.java
 # Usage
 
 1 - Add note
+
 2 - Remove note (with id)
+
 3 - Show note (with id)
+
 4 - Show existing notes' ids
+
 5 - Exit
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,36 @@
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static List<String> notes = new ArrayList<>();
+
     public static void main(String[] args) {
         System.out.println("Welcome to the Console Todo List!");
+        Scanner scanner = new Scanner(System.in);
+    }
 
+    public static void addNote(String note) {
+        notes.add(note);
+        int id = notes.indexOf(note);
+        System.out.println("Note #" + id + " has been successfully added!");
+    }
+
+    public static void removeNote(int id) {
+        notes.remove(id);
+        System.out.println("Note #" + id + " has been successfully removed!");
+    }
+
+    public static void showNote(int id) {
+        String note = notes.get(id);
+        System.out.println(note);
+    }
+
+    public static void showNotesIds() {
+        for(String note : notes) {
+            System.out.println(notes.indexOf(note));
+            System.out.println(note.substring(0, 10));
+            System.out.println();
+        }
     }
 }
